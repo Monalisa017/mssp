f661d0b4d0cf86e77f60fd2e3bcd3c99422eb794
Monalisa017 <61711279+Monalisa017@users.noreply.github.com>
2022-09-28 00:01:53 -0300

    feat: Adicionando Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { AppRoutes } from './routes';
 
 export const App = () => {
+  const notify = () => toast("Wow so easy!")
   return (
     <BrowserRouter>
       <AppRoutes />
+
+      <div>
+        <button onClick={notify}>Notify!</button>
+        <ToastContainer />
+      </div>
     </BrowserRouter>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,16 @@
+import Button from '@mui/material/Button/Button';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
 
 
 
 export const AppRoutes = () => {
+    function Teste(){
+        toast.success("Funcionando!")
+    }
     return(
         <Routes>
-            <Route path="/pagina-inicial" element={<p>Página Inicial</p>} />
+            <Route path="/pagina-inicial" element={<Button onClick={() => Teste()}>Teste Material</Button>} />
             <Route path="*" element={<Navigate to="pagina-inicial" />} />
         </Routes>
     );
