1faa9d4fe46330c184a60db8174c00123dba7f95
Monalisa017 <61711279+Monalisa017@users.noreply.github.com>
2022-10-11 00:15:41 -0300

    Corrigindo instalação EsLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React, { Component }  from 'react';
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
